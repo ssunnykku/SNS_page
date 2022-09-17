0efe2b7b99a725d641da98210889f95fb2dfc9e1
선희 김 <ssunnykku@gmail.com>
2022-09-17 20:03:09 +0900

    bootstrap menu 등록

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-
+      <Boards/>
     </div>
   );
 }
 
-function First() {
-  return(
-    <div>서니가함
-      또 수정함
-    </div>
-  )
+function Boards() {
+  return (
+    <>
+    <Nav variant="tabs" defaultActiveKey="link0">
+            <Nav.Item>
+              <Nav.Link eventKey="link1" >게시물</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link2">동영상</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link3">저장됨</Nav.Link>
+            </Nav.Item>
+        </Nav>
+        </>
+  );
 }
+
 export default App;
