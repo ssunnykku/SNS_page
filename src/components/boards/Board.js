38ca943236217e3abfe15c210abc2cf3778001df
선희 김 <ssunnykku@gmail.com>
2022-09-18 16:55:54 +0900

    comment css 작성중

--- src/components/boards/Board.js
+++ src/components/boards/Board.js
@@ -1,52 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function Board(){
-    return <>
-         <div className="board">
-            <article>
-                <div className="comment-header">
-                    <a href='#none'>
-                        <img className="board-img" src='/boardexm.jpg' alt="image" />
-                    </a>
+function Board() {
+  return (
+    <>
+      <div className="board">
+        <article>
+          <div className="comment-header">
+            <a href="#none">
+              <img className="board-img" src="/boardexm.jpg" alt="image" />
+            </a>
+          </div>
+          <div className="comment">
+            <div className="comment-header">
+              <div>
+                <a href="#none">
+                  <div className="comment-profile" alt="프로필 사진"></div>
+                </a>
+              </div>
+              <div className="comment-flex">
+                <div>
+                  <a herf="#none">
+                    <h6 className="nickname">heesoo</h6>
+                  </a>
+                  <a herf="#none">
+                    <span>서울시 무슨동 무슨카페</span>
+                  </a>
                 </div>
-                <div className="comment">
-                    <div className="comment-header">
-                        <a href='#none'>
-                            <div alt="프로필 사진"></div>
-                        </a>
-                        <div>
-                            <h4>닉넴</h4>
-                            <span>서울시 무슨동 무슨카페</span>
-                            <a href='#'></a>
-                        </div>
-                    </div>
-                    <div className="comment-header">
-                        <a href='#none'>
-                            <div alt="프로필 사진"></div>
-                        </a>
-                        <div>
-                            <h4>닉넴</h4>
-                            <span>서울시 무슨동 무슨카페</span>
-                            <a href='#'></a>
-                        </div>
-                    </div>
-                    <div className="comment-btn">
-                    <img src='/heart.svg' alt='btn-setting' />
-                        <img src='/chat.svg' alt='btn-setting' />
-                        <img src='/share.svg' alt='btn-setting' />
-                        <img src='/bookmark_border.svg' alt='btn-setting' />
-                        <span>sunny님 외 3명이 좋아합니다</span>
-                        <span>8월 4, 2021</span>
-                    </div>
-                    <div>
-                    <img src='/emoji.svg' alt='btn-setting' />
-                        <input type='text' placeholder='댓글 달기...'></input>
-                        <span type="button">게시</span>
-                    </div>
+                <div>
+                  <a href="#">...</a>
                 </div>
-            </article>
-        </div>
+              </div>
+            </div>
+            <div className="comment-chats">
+              <div>
+                <a href="#none">
+                  <div className="comment-profile" alt="프로필 사진"></div>
+                </a>
+              </div>
+              <div>
+                <a herf="#none">
+                  <h6 className="nickname">heesoo</h6>
+                </a>
+                <a herf="#none">
+                  <span>자주 만나자</span>
+                </a>
+                <span>58주</span>
+              </div>
+            </div>
+            <div className="comment-btn">
+              <div>
+                <img src="/favorite.svg" alt="btn-setting" />
+                <img src="/chat.svg" alt="btn-setting" />
+                <img src="/message.svg" alt="btn-setting" />
+              </div>
+              <div>
+                <img src="/bookmark_border.svg" alt="btn-setting" />
+              </div>
+            </div>
+            <div>
+              <h6>sunny님 외 3명이 좋아합니다</h6>
+              <span>8월 4, 2021</span>
+            </div>
+            <div>
+              <img src="/emoji.svg" alt="btn-setting" />
+              <input type="text" placeholder="댓글 달기..."></input>
+              <span type="button">게시</span>
+            </div>
+          </div>
+        </article>
+      </div>
     </>
-};
+  );
+}
 
 export default Board;
