a58db825bee1da00c9d91c01499d42e3f45f91e4
선희 김 <ssunnykku@gmail.com>
2022-09-19 02:26:58 +0900

    emoji 삽입

--- src/components/boards/Board.js
+++ src/components/boards/Board.js
@@ -1,75 +1,89 @@
 import React, { useState } from "react";
 
-function Board() {
+function Board({ board, setBoard }) {
   return (
-    <div className="comment-back">
-      <div className="board">
-        <article>
-          <div className="comment-header">
-            <a href="#none">
-              <img className="board-img" src="/boardexm.jpg" alt="image" />
-            </a>
-          </div>
-          <div className="comment">
+    <>
+      <div className="comment-back">
+        <img src="/arrow_circle_left.svg" alt="btn-setting" />
+        <div className="board">
+          <article>
             <div className="comment-header">
-              <div>
-                <a href="#none">
-                  <div className="comment-profile" alt="프로필 사진"></div>
-                </a>
+              <a href="#none">
+                <img className="board-img" src="/boardexm.jpg" alt="image" />
+              </a>
+            </div>
+            <div className="comment">
+              <div className="comment-header">
+                <div>
+                  <a href="#none">
+                    <div className="comment-profile" alt="프로필 사진"></div>
+                  </a>
+                </div>
+                <div className="comment-flex">
+                  <div>
+                    <a herf="#none">
+                      <h6 className="nickname">heesoo</h6>
+                    </a>
+                    <a herf="#none">
+                      <span>서울시 무슨동 무슨카페</span>
+                    </a>
+                  </div>
+                  <div>
+                    <img src="/more_horiz_black.svg" alt="btn-setting" />
+                  </div>
+                </div>
               </div>
-              <div className="comment-flex">
+              <div className="comment-chats">
+                <div>
+                  <a href="#none">
+                    <div className="comment-profile" alt="프로필 사진"></div>
+                  </a>
+                </div>
                 <div>
                   <a herf="#none">
                     <h6 className="nickname">heesoo</h6>
                   </a>
                   <a herf="#none">
-                    <span>서울시 무슨동 무슨카페</span>
+                    <span>자주 만나자</span>
                   </a>
+                  <span>58주</span>
+                </div>
+              </div>
+              <div className="comment-btn">
+                <div>
+                  <img src="/favorite.svg" alt="btn-setting" />
+                  <img src="/chat.svg" alt="btn-setting" />
+                  <img src="/message.svg" alt="btn-setting" />
                 </div>
                 <div>
-                  <a href="#">...</a>
+                  <img src="/bookmark_border.svg" alt="btn-setting" />
                 </div>
               </div>
-            </div>
-            <div className="comment-chats">
               <div>
-                <a href="#none">
-                  <div className="comment-profile" alt="프로필 사진"></div>
-                </a>
+                <h6>sunny님 외 3명이 좋아합니다</h6>
+                <span>8월 4, 2021</span>
               </div>
               <div>
-                <a herf="#none">
-                  <h6 className="nickname">heesoo</h6>
-                </a>
-                <a herf="#none">
-                  <span>자주 만나자</span>
-                </a>
-                <span>58주</span>
+                <img src="/emoji.svg" alt="btn-setting" />
+                <input type="text" placeholder="댓글 달기..."></input>
+                <span type="button">게시</span>
               </div>
             </div>
-            <div className="comment-btn">
-              <div>
-                <img src="/favorite.svg" alt="btn-setting" />
-                <img src="/chat.svg" alt="btn-setting" />
-                <img src="/message.svg" alt="btn-setting" />
-              </div>
-              <div>
-                <img src="/bookmark_border.svg" alt="btn-setting" />
-              </div>
-            </div>
-            <div>
-              <h6>sunny님 외 3명이 좋아합니다</h6>
-              <span>8월 4, 2021</span>
-            </div>
-            <div>
-              <img src="/emoji.svg" alt="btn-setting" />
-              <input type="text" placeholder="댓글 달기..."></input>
-              <span type="button">게시</span>
-            </div>
-          </div>
-        </article>
+          </article>
+        </div>
+        <div id="btns">
+          <img
+            id="close"
+            src="/close.svg"
+            alt="btn-setting"
+            onClick={() => {
+              setBoard(!board);
+            }}
+          />
+          <img src="/arrow_circle_right.svg" alt="btn-setting" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/boards/TabContent.js
+++ src/components/boards/TabContent.js
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
-import Board from './Board';
+import React, { useState } from "react";
+import Board from "./Board";
 
 function TabContent({ tab, photos }) {
-    const [board, setBoard] = useState(false);
+  const [board, setBoard] = useState(false);
 
-    if (tab === 0) {
-        return (<>
-            <ul className='tab-container'>
-               { photos.map((a, i) => {
-                    return (
-                    <li className='photoList' key={photos[i].id}>
-                    <a href='#none'>
-                       <img className='photoList' src={photos[i].photo} alt='boardImg' type="button" onClick={()=>{setBoard(!board)}} />
-                    </a>
-                </li>)
-                })}
-            </ul>
-              { board ? 
-                <div><Board /></div> : null }
-              </>
-        );
-    } else if (tab === 1) {
-        return <div>내용1</div>;
-    } else if (tab === 2) {
-        return <div>내용1</div>;
-    }
+  if (tab === 0) {
+    return (
+      <>
+        <ul className="tab-container">
+          {photos.map((a, i) => {
+            return (
+              <li className="photoList" key={photos[i].id}>
+                <a href="#none">
+                  <img
+                    className="photoList"
+                    src={photos[i].photo}
+                    alt="boardImg"
+                    type="button"
+                    onClick={() => {
+                      setBoard(!board);
+                    }}
+                  />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        {board ? (
+          <div>
+            <Board board={board} setBoard={setBoard} />
+          </div>
+        ) : null}
+      </>
+    );
+  } else if (tab === 1) {
+    return <div>내용1</div>;
+  } else if (tab === 2) {
+    return <div>내용1</div>;
+  }
 }
 
 export default TabContent;
